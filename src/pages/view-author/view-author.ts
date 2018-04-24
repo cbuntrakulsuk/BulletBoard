0b051679235870215b82ac502945b3072c0d35d7
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2018-04-23 20:46:55 -0700

    Set up view author page

--- src/pages/view-author/view-author.ts
+++ src/pages/view-author/view-author.ts
@@ -1,25 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ViewAuthorPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-view-author',
   templateUrl: 'view-author.html',
 })
 export class ViewAuthorPage {
+  author: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ViewAuthorPage');
+    this.author = navParams.get('data');
   }
 
 }
--- src/pages/view-post/view-post.ts
+++ src/pages/view-post/view-post.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { ViewAuthorPage } from '../view-author/view-author';
+
 @IonicPage()
 @Component({
   selector: 'page-view-post',
@@ -11,6 +13,12 @@ export class ViewPostPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.post = navParams.get('data');
+  }
+
+  viewAuthor(event: any, author: any):void {
+    this.navCtrl.push(ViewAuthorPage, {
+      data: author
+    });
   }
 
 }
