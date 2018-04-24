ca300201cd3902139846365a4dd2eb209ab611a0
egadly <ernest.gaddi.491@my.csun.edu>
2018-04-23 20:06:15 -0700

    quick fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -57,7 +57,7 @@ import { LocationPage } from '../pages/location/location';
     TabsPage,
     ViewPostPage,
     ViewAuthorPage,
-    LocationPage
+    LocationPage,
 		SendPage
   ],
   providers: [
