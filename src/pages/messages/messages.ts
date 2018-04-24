820d6022580f7139efee8cc74b45fbb83d774d02
Lemuel Dizon <lemuel.dizon.703@my.csun.edu>
2018-04-23 18:19:03 -0700

    Merging

---
+++ src/pages/messages/messages.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-messages',
+  templateUrl: 'messages.html'
+})
+
+export class Messages1 {
+
+  constructor(public navCtrl: NavController) {
+	}
+
+}
